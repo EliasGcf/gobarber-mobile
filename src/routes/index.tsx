9b9caf5c83409c10551b7f48184c1b8502f6dc12
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-07-13 21:13:29 -0300

    feat: add app icon and splashscreen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import React, { useEffect } from 'react';
+// import { ActivityIndicator, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
@@ -9,13 +10,19 @@ import { useAuth } from '../hooks/auth';
 const Routes: React.FC = () => {
   const { user, loading } = useAuth();
 
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#999" />
-      </View>
-    );
-  }
+  useEffect(() => {
+    if (!loading) {
+      SplashScreen.hide();
+    }
+  }, [loading]);
+
+  // if (loading) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  //       <ActivityIndicator size="large" color="#999" />
+  //     </View>
+  //   );
+  // }
 
   return user ? <AppRoutes /> : <AuthRoutes />;
 };
